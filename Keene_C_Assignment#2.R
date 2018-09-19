708c4289d38d190a9cccd87d358bb913de8b696f
Cheri Trahan Keene <42742796+ctrahankeene@users.noreply.github.com>
2018-09-18 21:07:53 -0500

    Add files via upload

--- Keene_C_Assignment#2.R
+++ Keene_C_Assignment#2.R
@@ -4,15 +4,9 @@ Assignment_2.data<-read.csv("C:/Users/Cheri/Documents/Professional/PHD Classes/s
 subAssign_2 <-Assignment_2.data[1:191, 1:12]
 #identify list of variables
 print(subAssign_2 [1,1:12])
-#mean(subAssign_2[1:191, 2],na.rm = TRUE)
-#str(subAssign_2)
-library(magrittr)
-library(dplyr)
-#subAssign_2<-as.numeric(subAssign_2)
+#library(magrittr)
+#library(dplyr)
 subAssign_2 <- as.data.frame(sapply(subAssign_2, as.numeric))
-
-str(subAssign_2)
-View(subAssign_2)
 #mean of 5 variables
 mean(subAssign_2$Total.Membership)
 mean(subAssign_2$Convert.Baptisms)
@@ -24,16 +18,30 @@ median(subAssign_2$Convert.Baptisms)
 median(subAssign_2$Total.Membership)
 median(subAssign_2$Mormon.Birth.Rate)
 median(subAssign_2$Mormon.Life.Expectancy)
-median(subAssign_2$Mormon.Birth.Rate)
 median(subAssign_2$Mormon.Death.Rate)
 #mode of 5 variables
 temp1<-table(as.vector(subAssign_2$Total.Membership))
 names(temp1[temp1 == max(temp1)])
 temp2<-table(as.vector(subAssign_2$Convert.Baptisms))
-names(temp2[temp2 == max(temp)])
+names(temp2[temp2 == max(temp2)])
 temp3<-table(as.vector(subAssign_2$Mormon.Birth.Rate))
-names(temp3[temp3 == max(temp)])
+names(temp3[temp3 == max(temp3)])
 temp4<-table(as.vector(subAssign_2$Mormon.Death.Rate))
-names(temp4[temp == max(temp4)])
+names(temp4[temp4 == max(temp4)])
 temp5<-table(as.vector(subAssign_2$Mormon.Life.Expectancy))
-names(temp5[temp5 == max(temp)])
+names(temp5[temp5 == max(temp5)])
+#find variance, range, and standard deviation for one variable
+sd(subAssign_2$Mormon.Birth.Rate)
+var(subAssign_2$Mormon.Birth.Rate)
+range(subAssign_2$Mormon.Birth.Rate)
+max(subAssign_2$Mormon.Birth.Rate)-min(subAssign_2$Mormon.Birth.Rate)
+#view temp files to determine if mode exists
+View(temp1)
+View(temp2)
+View(temp3)
+View(temp4)
+View(temp5)
+#create histogram of one variable
+hist(subAssign_2$Mormon.Life.Expectancy)
+#create stem-leaf plot of one variable
+stem(subAssign_2$Mormon.Life.Expectancy)
